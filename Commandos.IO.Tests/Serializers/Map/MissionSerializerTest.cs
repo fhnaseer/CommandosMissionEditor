46bdf86b3b15740d4173957dd8b9c04645ed59cf
fhnaseer <fhnaseer@live.com>
2018-12-17 17:43:25 +0100

    Fixing unit test,

--- Commandos.IO.Tests/Serializers/Map/MissionSerializerTest.cs
+++ Commandos.IO.Tests/Serializers/Map/MissionSerializerTest.cs
@@ -14,7 +14,8 @@ namespace Commandos.IO.Tests.Serializers.Map
         public void GetMission_Works()
         {
             // Arrange,
-            const string text = "[ .MANUAL_LIBRETA Manual_Libreta_TU01A.msb .VISORES [ .XYZ ( 1 2 0 ) .ESC EXTERIOR .CAMARA 0 ] .BRIEFING [ .INICIAL TU01A.BRI ] .BAS TU01.BAS ]";
+            const string text = "[ .MANUAL_LIBRETA Manual_Libreta_TU01A.msb .VISORES [ .XYZ ( 1 2 0 ) .ESC EXTERIOR .CAMARA 0 ] .BRIEFING [ .INICIAL TU01A.BRI ] .FICHEROS [ .STR TU1A.STR ] .BAS TU01.BAS" +
+                " .MUSICA  [ .MUSICAS (  ( M_BU_Ext.WAV EXTERIOR ) ( M_BU_Agu.WAV AGUA ) ( M_BU_Int.WAV INTERIOR ) ) .MUSICA_POR_DEFECTO INTERIOR ] ]";
             var tokens = text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             var expected = new Mission
             {
@@ -30,7 +31,19 @@ namespace Commandos.IO.Tests.Serializers.Map
                 {
                     FileName = "TU01A.BRI"
                 },
+                Ficheros = new Ficheros
+                {
+                    FileName = "TU1A.STR"
+                }
             };
+            var music = new Music
+            {
+                StartingMusicEnvironment = "INTERIOR"
+            };
+            music.BackgroundMusics.Add(new Model.Common.BackgroundMusic { Environment = "EXTERIOR", MusicFileName = "M_BU_Ext.WAV" });
+            music.BackgroundMusics.Add(new Model.Common.BackgroundMusic { Environment = "AGUA", MusicFileName = "M_BU_Agu.WAV" });
+            music.BackgroundMusics.Add(new Model.Common.BackgroundMusic { Environment = "INTERIOR", MusicFileName = "M_BU_Int.WAV" });
+            expected.Music = music;
             var record = TokenParser.ParseTokens(tokens);
 
             // Act,
