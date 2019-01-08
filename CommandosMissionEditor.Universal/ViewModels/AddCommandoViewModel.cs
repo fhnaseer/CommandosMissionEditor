dba9781e0c4751d84036337bb71f46b256d8c652
fhnaseer <fhnaseer@live.com>
2019-01-08 23:40:42 +0100

    Adding AddCommandoPage,

--- CommandosMissionEditor.Universal/ViewModels/AddCommandoViewModel.cs
+++ CommandosMissionEditor.Universal/ViewModels/AddCommandoViewModel.cs
@@ -1,16 +1,38 @@
-﻿using System.Windows.Input;
-using CommandosMissionEditor.Universal.Helpers;
+﻿using System;
+using System.Collections.ObjectModel;
+using Commandos.Model.Characters.Commandos;
 
 namespace CommandosMissionEditor.Universal.ViewModels
 {
-    public class AddCommandoViewModel : ViewModelBase
+    public class AddCommandoViewModel : AddItemViewModelBase<Commando>
     {
+        public override ObservableCollection<Commando> ItemCollection => Mission.World.MissionObjects.Commandos;
 
-        private ICommand _addCommand;
-        public ICommand AddCommand => _addCommand ?? (_addCommand = new RelayCommand(Add));
-
-        internal void Add()
+        private ObservableCollection<Commando> _commandos;
+        public ObservableCollection<Commando> Commandos => _commandos ?? (_commandos = new ObservableCollection<Commando>
         {
+            new GreenBeret(),
+            new Sniper(),
+            new Marine(),
+            new Sapper(),
+            new Driver(),
+            new Spy(),
+            new Natasha(),
+            new Thief()
+        });
+
+        private Commando _selectedCommando;
+        public Commando SelectedCommando
+        {
+            get => _selectedCommando;
+            set => Set(ref _selectedCommando, value);
+        }
+
+        internal override void AddItem()
+        {
+            var commando = Activator.CreateInstance(SelectedCommando.GetType()) as Commando;
+            ItemCollection.Add(commando);
+            SelectedItem = commando;
         }
     }
 }
