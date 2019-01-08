ba39ed2af970796f9cb381d75d40537af5de6af0
fhnaseer <fhnaseer@live.com>
2019-01-08 21:40:41 +0100

    Populating MusicPage, Button enable is not being updated from viewmodel, disable IsEnabled check,

--- CommandosMissionEditor.Universal/ViewModels/MusicViewModel.cs
+++ CommandosMissionEditor.Universal/ViewModels/MusicViewModel.cs
@@ -1,13 +1,26 @@
-﻿using System;
-
-using CommandosMissionEditor.Universal.Helpers;
+﻿using System.Collections.ObjectModel;
+using Commandos.Model.Common;
 
 namespace CommandosMissionEditor.Universal.ViewModels
 {
-    public class MusicViewModel : ViewModelBase
+    public class MusicViewModel : AddItemViewModelBase<BackgroundMusic>
     {
         public MusicViewModel()
         {
+        }
+
+        public override ObservableCollection<BackgroundMusic> ItemCollection => Mission.Music.BackgroundMusics;
+
+        public override void LoadDefaultItem()
+        {
+            foreach (var item in ItemCollection)
+                if (item.Environment == Mission.Music.StartingMusicEnvironment)
+                    DefaultItem = item;
+        }
+
+        public override void OnDefaultItemChanged()
+        {
+            Mission.Music.StartingMusicEnvironment = DefaultItem.Environment;
         }
     }
 }
